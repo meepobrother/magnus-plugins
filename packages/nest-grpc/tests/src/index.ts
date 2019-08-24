e26a868c6be879348cdbefe2a1a6058b4683d9a4
meepo <1037483576@qq.com>
2019-08-24 14:40:17 +0800

    fix: 🐛 fix error

--- packages/nest-grpc/tests/src/index.ts
+++ packages/nest-grpc/tests/src/index.ts
@@ -1,8 +1,9 @@
-import { NestGrpc } from "../../lib/index";
+import { bootstrap } from "../../lib/index";
 import { join } from "path";
 import { TestMagnus } from "./inc/test";
 const metadata = require("./assets/fastity/magnus.metadata.json");
-const grpc = new NestGrpc({
+import { AppModule } from "./app";
+bootstrap(AppModule, {
   url: `0.0.0.0:3004`,
   protoPath: join(__dirname, "assets/fastity/magnus.server.proto"),
   package: `fastity`,
@@ -11,7 +12,4 @@ const grpc = new NestGrpc({
     TestMagnus
   },
   decorators: {}
-});
-grpc.listen(() => {
-  console.log(`start`);
 });
