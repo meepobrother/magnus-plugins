1f0740c5354c9c9f9898610311d1c54f0c0403b0
imeepos <1037483576@qq.com>
2019-09-03 09:58:26 +0800

    fix: 🐛 fix nest resolver

---
+++ packages/nest-resolver/__tests__/nest-resolver.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const nestResolver = require('..');
+
+describe('nest-resolver', () => {
+    it('needs tests');
+});
---
+++ packages/nest-resolver/lib/basic.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const lodash_1 = require("lodash");
+class BaseExplorerService {
+    getModules(modulesContainer, include) {
+        if (!include || lodash_1.isEmpty(include)) {
+            return [...modulesContainer.values()];
+        }
+        const whitelisted = this.includeWhitelisted(modulesContainer, include);
+        return whitelisted;
+    }
+    includeWhitelisted(modulesContainer, include) {
+        return [...modulesContainer.values()].filter(({ metatype }) => include.some(item => item === metatype));
+    }
+}
+exports.BaseExplorerService = BaseExplorerService;
