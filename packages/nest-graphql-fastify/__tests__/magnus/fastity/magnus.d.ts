e0e60d51d7f3ab2f237f566e7b188bf4990b895a
imeepos <1037483576@qq.com>
2019-09-03 10:32:56 +0800

    fix: 🐛 remove test

--- packages/nest-graphql-fastify/__tests__/magnus/fastity/magnus.d.ts
+++
@@ -1,3 +0,0 @@
-export interface Query {
-    add<T>(a: number, b: number, __selection?: string): Promise<T & number>;
-}
--- packages/nest-graphql-fastify/__tests__/resolver/inc.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class IncController {
-    add(a: number, b: number, headers: any): number;
-}
